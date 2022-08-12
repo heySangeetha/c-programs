f783374d415906aa8fff51f7bf597e24bfd75637
heySangeetha <109811455+heySangeetha@users.noreply.github.com>
2022-08-12 23:00:36 +0530

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -1,26 +1,23 @@
 #include<stdio.h>
 void main()
 {
-	int i,j,n,k,l
-	;
+	int i,j,n;
 	printf("enter n");
 	scanf("%d",&n);
 	for(i=1;i<n;i++)
 	{
-		    for(j=1;j<=i;j++)
+	        for(j=1;j<=i;j++)
 	    	{
-				printf("*");
-	    	    printf("\n");
-	        }
-			for(i=n;i>=0;i--)
+	            printf("*");
+		}
+	        printf("\n");
+	}  
+        for(i=n-1;i>=1;i--)
+	{
+	        for(j=1;j<=i;j++)
 	        {
-	        	for(j=1;j<=i;j++)
-	        	{
 	    	    	printf("*");
-		     	}
-	            printf("\n");
-         	}
-     }
-	
-
+		}
+	        printf("\n");
+        }
 }
