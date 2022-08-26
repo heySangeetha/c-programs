364c26f44a252e5a32468adc09a4cad5201b8e24
heySangeetha <109811455+heySangeetha@users.noreply.github.com>
2022-08-26 22:15:28 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{ 
+	char s[200];
+	int i=0,j=0,k,size,flag;
+	printf("Enter a string:");
+	gets(s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		k++;
+	}
+	size=k;
+	
+	while(i<(k/2))
+    {   
+        if(s[i]!=s[j])
+        {
+	        flag=1;
+            break;
+        }
+    	i++;
+    	j--;
+    }
+	if(flag==0)
+	printf("palindrome");
+	else
+	printf("not palindrome");
+	
+}
